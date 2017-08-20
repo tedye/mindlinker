fecbb1d87a7a3a7947c98abb7f2b9e9fd28ed139
Ted Ye <yefangliang1226@gmail.com>
2017-08-20 13:09:05 -0700

    index.php no-cache

--- index.php
+++ index.php
@@ -1,40 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <title>Mindlinker v0.1</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
-    <meta name="HandheldFriendly" content="True">
-    <meta name="MobileOptimized" content="320">
-    <meta http-equiv="cleartype" content="on">
-    <link rel="icon" href="assets/images/knight/knight_story_image.png">
-    <script src="assets/blocks/js/blockly_compressed_horizontal.js"></script>
-    <script src="assets/blocks/js/blocks_compressed_horizontal.js"></script>
-    <script src="assets/blocks/js/blocks_compressed.js"></script>
-    <script src="assets/blocks/js/javascript_compressed.js"></script>
-    <script src="assets/blocks/js/zh-hans.js"></script>
-    <script src="assets/blocks/js/custom_block_defs.js"></script>
-    <script src="assets/blocks/js/custom_code_generators.js"></script>
-    <script src="assets/blocks/js/colour.js"></script>
-    <script src="assets/blocks/js/text.js"></script>
-    <script src="assets/blocks/js/math.js"></script>
-    <style>
-        html,
-        body {
-            margin: 0;
-            padding: 0;
-        }
-    </style>
-</head>
-
-<body>
-
-    <div id="content"></div>
-    <div id="block" style="position: absolute;top: 655px;height: 400px;right: 0px;left: 0px;bottom: 0px;opacity: 0.9"></div>
-    <script src="dist/vendor.bundle.js"></script>
-    <script src="dist/bundle.js"></script>
-
-</body>
-
-</html>
+<?php
+    header('Location: /index.html');
+    header("Cache-Control: no-cache, must-revalidate");
+?>
